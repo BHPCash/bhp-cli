2bba89ab6799dfc34bbefbfbaf845b90bee16883
bhp <github@bhpcash.io>
2018-06-15 17:50:56 +0800

    Initial commit

---
+++ bhp-cli/Settings.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Extensions.Configuration;
+using Bhp.Network;
+
+namespace Bhp
+{
+    internal class Settings
+    {
+        public PathsSettings Paths { get; }
+        public P2PSettings P2P { get; }
+        public RPCSettings RPC { get; }
+        public UnlockWalletSettings UnlockWallet { get; set; }
+
+        public static Settings Default { get; }
+
+        static Settings()
+        {
+            IConfigurationSection section = new ConfigurationBuilder().AddJsonFile("config.json").Build().GetSection("ApplicationConfiguration");
+            Default = new Settings(section);
+        }
+
+        public Settings(IConfigurationSection section)
+        {
+            this.Paths = new PathsSettings(section.GetSection("Paths"));
+            this.P2P = new P2PSettings(section.GetSection("P2P"));
+            this.RPC = new RPCSettings(section.GetSection("RPC"));
+            this.UnlockWallet = new UnlockWalletSettings(section.GetSection("UnlockWallet"));
+        }
+    }
+
+    internal class PathsSettings
+    {
+        public string Chain { get; }
+        public string ApplicationLogs { get; }
+
+        public PathsSettings(IConfigurationSection section)
+        {
+            this.Chain = string.Format(section.GetSection("Chain").Value, Message.Magic.ToString("X8"));
+            this.ApplicationLogs = string.Format(section.GetSection("ApplicationLogs").Value, Message.Magic.ToString("X8"));
+        }
+    }
+
+    internal class P2PSettings
+    {
+        public ushort Port { get; }
+        public ushort WsPort { get; }
+
+        public P2PSettings(IConfigurationSection section)
+        {
+            this.Port = ushort.Parse(section.GetSection("Port").Value);
+            this.WsPort = ushort.Parse(section.GetSection("WsPort").Value);
+        }
+    }
+
+    internal class RPCSettings
+    {
+        public ushort Port { get; }
+        public string SslCert { get; }
+        public string SslCertPassword { get; }
+
+        public RPCSettings(IConfigurationSection section)
+        {
+            this.Port = ushort.Parse(section.GetSection("Port").Value);
+            this.SslCert = section.GetSection("SslCert").Value;
+            this.SslCertPassword = section.GetSection("SslCertPassword").Value;
+        }
+    }
+
+    public class UnlockWalletSettings
+    {
+        public string Path { get; }
+        public string Password { get; }
+        public bool StartConsensus { get; }
+        public bool IsActive { get; }
+
+        public UnlockWalletSettings(IConfigurationSection section)
+        {
+            if (section.Value != null)
+            {
+                this.Path = section.GetSection("WalletPath").Value;
+                this.Password = section.GetSection("WalletPassword").Value;
+                this.StartConsensus = bool.Parse(section.GetSection("StartConsensus").Value);
+                this.IsActive = bool.Parse(section.GetSection("IsActive").Value);
+            }
+        }
+    }
+}
